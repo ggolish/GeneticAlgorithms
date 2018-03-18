e2595a1fea118037f8c5ad4b930fc0df810eebf6
ggolish <ggolish@sycamores.indstate.edu>
2018-03-18 01:38:34 -0400

    Initial repo creation

---
+++ genetics.py
@@ -0,0 +1,63 @@
+
+import random
+
+class Genetics(object):
+
+    def __init__(self, population_size, chromosome_size, min_gene, max_gene, crossover_rate = 0.7, mutation_rate = 0.1):
+        self.population_size = population_size
+        self.chromosome_size = chromosome_size
+        self.min_gene = min_gene
+        self.max_gene = max_gene
+        self.crossover_rate = crossover_rate
+        self.mutation_rate = mutation_rate
+        self.fitnesses = []
+        self.population = self.new_population()
+        self.generation = 1
+
+    def new_population(self):
+        return [self.new_chromosome() for _ in range(self.population_size)]
+
+    def new_chromosome(self):
+        return [self.new_gene() for _ in range(self.chromosome_size)]
+
+    def new_gene(self):
+        return random.randint(self.min_gene, self.max_gene)
+
+    def repopulate(self):
+        new_population = []
+        self.total_fitness = sum(self.fitnesses)
+        if self.total_fitness <= 0: return
+        self.probabilities = [f / self.total_fitness for f in self.fitnesses]
+        while len(new_population) != len(self.population):
+            c1, c2 = self.choose(), self.choose()
+            self.crossover(c1, c2)
+            self.mutate([c1, c2])
+            new_population.extend([c1, c2])
+        self.population = new_population
+        self.generation += 1
+
+    def choose(self):
+        r = random.uniform(0, 1)
+        index = -1
+        while r > 0:
+            index += 1
+            r -= self.probabilities[index]
+        return list(self.population[index])
+
+    def crossover(self, c1, c2):
+        prob = random.random()
+        if prob < self.crossover_rate:
+            index = random.randint(0, self.chromosome_size - 1)
+            copy1 = list(c1)
+            copy2 = list(c2)
+            for i in range(index):
+                c1[i] = copy2[i]
+            for i in range(index, self.chromosome_size):
+                c2[i] = copy1[i]
+
+    def mutate(self, chromosomes):
+        prob = random.random()
+        if prob < self.mutation_rate:
+            for c in chromosomes:
+                c[random.randint(0, self.chromosome_size - 1)] = self.new_gene()
+
---
+++ shakespeare.py
@@ -0,0 +1,36 @@
+
+from genetics import Genetics
+
+target = "to be or not to be that is the question"
+alphabet = "abcdefghijklmnopqrstuvwxyz "
+
+def calcFitness(genalg):
+    genalg.fitnesses = []
+    for c in genalg.population:
+        s = ""
+        f = 0
+        for g in c:
+            s += alphabet[g]
+        for i in range(len(target)):
+            if s[i] == target[i]:
+                f += 1
+        genalg.fitnesses.append(f * f)
+
+
+if __name__ == "__main__":
+
+    genalg = Genetics(200, len(target), 0, 26)
+    while True:
+        calcFitness(genalg)
+        f, c = max(zip(genalg.fitnesses, genalg.population))
+        print("{:02d}".format(f), end=" ")
+        for g in c:
+            print(alphabet[g], end="")
+        if f == len(target) * len(target):
+            print()
+            break
+        else:
+            print(end="\r", flush=True)
+        genalg.repopulate()
+    print(genalg.generation, "generations")
+        
